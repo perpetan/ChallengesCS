6050154a0c9989f104647b37290422b0b81d5e8d
Vinícius Mendonça <vini066@gmail.com>
2021-01-15 23:44:43 -0300

    MaximumConsecutiveNumbers

--- MaximumConsecutiveNumbers.cs
+++ MaximumConsecutiveNumbers.cs
@@ -6,55 +6,58 @@ For example, having the next array: [1,1,0,1,1,1], the maximum number of consecu
 using System;
 using System.Linq;
 
-static void Main(string[] args)
+class Program
 {
-    int[] input = { 1, 1, 0, 0, 0, 0, 0, 1, 1, 1 };
-    int numberToBeCounted = 1;
-
-    var result = ConsecutiveNumbers(input);
-    Console.WriteLine(result.Item1 + " consecutive " + result.Item2 + "s found");
-
-    Console.WriteLine(ConsecutiveNumbers(input, numberToBeCounted) + " consecutive " + numberToBeCounted + "s found");
-}
-
-// This method compares all array positions and give the maximum number of any consecutive number.
-// Note that it will return the consecutive value AND the number which is consecutive
-public static (int, int) ConsecutiveNumbers(int[] numbers)
-{
-    int count = 1;
-    int consecutive = 1;
-
-    int consecutiveNumber = numbers.First();
-
-    for (int i=1; i<numbers.Length; i++)
+    static void Main(string[] args)
     {
-        _ = numbers[i] == numbers[i - 1] ? count++ : count = 1;
+        int[] input = { 1, 1, 0, 0, 0, 0, 0, 1, 1, 1 };
+        int numberToBeCounted = 1;
 
-        if (count >= consecutive)
+        var result = ConsecutiveNumbers(input);
+        Console.WriteLine(result.Item1 + " consecutive " + result.Item2 + "s found");
+
+        Console.WriteLine(ConsecutiveNumbers(input, numberToBeCounted) + " consecutive " + numberToBeCounted + "s found");
+    }
+
+    // This method compares all array positions and give the maximum number of any consecutive number.
+    // Note that it will return the consecutive value AND the number which is consecutive
+    public static (int, int) ConsecutiveNumbers(int[] numbers)
+    {
+        int count = 1;
+        int consecutive = 1;
+
+        int consecutiveNumber = numbers.First();
+
+        for (int i=1; i<numbers.Length; i++)
         {
-            consecutive = count;
-            consecutiveNumber = numbers[i];
+            _ = numbers[i] == numbers[i - 1] ? count++ : count = 1;
+
+            if (count >= consecutive)
+            {
+                consecutive = count;
+                consecutiveNumber = numbers[i];
+            }
         }
+
+        return (consecutive, consecutiveNumber);
     }
 
-    return (consecutive, consecutiveNumber);
-}
-
-// This method give the maximum number of consecutive number entered in code.
-// It only returns how many times the given number is consecutive.
-// It does not return the consecutive number itself, because it is already declared in numberToBeCounted variable in Main()
-public static int ConsecutiveNumbers(int[] numbers, int numberToBeCounted)
-{
-    int count = 1;
-    int consecutive = 1;
-
-    foreach (int number in numbers)
+    // This method give the maximum number of consecutive number entered in code.
+    // It only returns how many times the given number is consecutive.
+    // It does not return the consecutive number itself, because it is already declared in numberToBeCounted variable in Main()
+    public static int ConsecutiveNumbers(int[] numbers, int numberToBeCounted)
     {
-        _ = number == numberToBeCounted ? count++ : count = 1;
+        int count = 1;
+        int consecutive = 1;
 
-        if (count >= consecutive)
-            consecutive = count;
+        foreach (int number in numbers)
+        {
+            _ = number == numberToBeCounted ? count++ : count = 1;
+
+            if (count >= consecutive)
+                consecutive = count;
+        }
+
+        return consecutive;
     }
-
-    return consecutive;
 }
